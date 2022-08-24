657e6c1eba0f62ca248e62f72bcd6ef159faa006
Rafael Cortés <107086728+racortes91@users.noreply.github.com>
2022-08-24 14:45:51 -0400

    página con carrito de compra

--- js/app.js
+++ js/app.js
@@ -1,27 +1,57 @@
+const carrito = [];
 
-const ussers = []
-let persona1 = { name: "pepe",  pass: "pepe1234"}
-   ussers.push ("persona1");
-   
-   console.log('Usuario: ', persona1.name);
-   console.log('Contraseña: ', persona1.pass);
+class StatusCarrito {
+   constructor (nombre, precio, imagen, id){
+      this.nombre = nombre;
+      this.precio = precio;
+      this.imagen= imagen;
+      this.id = id;
+   }
+}
 
-let btn = document.getElementById('formBtn');
-btn.addEventListener('click', (e) =>{
-   e.preventDefault()
-   
-   let loginUsser = document.getElementById('formUsser').value;
-   let loginPass = document.getElementById('formPass').value;
-  
-   if (loginUsser == persona1.name && loginPass == persona1.pass)
-      { 
-      window.location.href="https://racortes91.github.io/proyecto-final/";
-      }
-      else
-      {
-      document.getElementById('usserTxt').innerHTML="Usuario Incorrecto"  
-      document.getElementById('passTxt').innerHTML="Contraseña Incorrecta"
-      }
-});
+let contenerdoConsolas = document.getElementById("contenedorProducto");
 
-   
+function cardsProductos(arrayConsolas) {
+   for(let consolas of arrayConsolas) {
+      let div = document.createElement("div");  
+
+      div.className = 'consola__card-ps5';
+
+      div.innerHTML = `<div class="consola__card">
+                           <img src=" ${consolas.img}" alt="${consolas.id}">
+                           <h2 class="titulo__producto">${consolas.nombre}</h2>
+                           <p class="precio__producto">$${consolas.precio}</p>
+                           <button id="btnAgregar" class="boton__productos">
+                              Agregar
+                           </button>
+                        </div> `
+
+      contenerdoConsolas.appendChild(div);
+ }
+}
+cardsProductos(consolas);
+
+let botones = document.querySelectorAll(".boton__productos");  
+
+botones.forEach(elemento => {
+   elemento.addEventListener("click", agregarCarrito);
+})
+
+function agregarCarrito(e) {
+
+   let carritoLocal = JSON.parse(localStorage.getItem("carrito"));
+
+   if(carritoLocal){
+      carrito = carritoLocal;
+   }
+
+   let imagen = e.target.parentNode.children[0].src;
+   let nombre = e.target.parentNode.children[1].textContent;
+   let precio = e.target.parentNode.children[2].textContent;  
+   let id = e.target.parentNode.children[0].alt;
+
+   const productoConsolas = new StatusCarrito(imagen, nombre, precio, id);
+   carrito.push(productoConsolas);
+
+   localStorage.setItem("carrito", JSON.stringify(carrito));
+}
---
+++ js/login.js
@@ -0,0 +1,27 @@
+
+const ussers = []
+let persona1 = { name: "pepe",  pass: "pepe1234"}
+   ussers.push ("persona1");
+   
+   console.log('Usuario: ', persona1.name);
+   console.log('Contraseña: ', persona1.pass);
+
+let btn = document.getElementById('formBtn');
+btn.addEventListener('click', (e) =>{
+   e.preventDefault();
+   
+   let loginUsser = document.getElementById('formUsser').value;
+   let loginPass = document.getElementById('formPass').value;
+  
+   if (loginUsser == persona1.name && loginPass == persona1.pass)
+      { 
+      window.location.href="https://racortes91.github.io/proyecto-final/";
+      }
+      else
+      {
+      document.getElementById('usserTxt').innerHTML="Usuario Incorrecto"  
+      document.getElementById('passTxt').innerHTML="Contraseña Incorrecta"
+      }
+});
+
+   
