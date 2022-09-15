3e1fc182290e82d9fc9edbec4977a41bf13eb54f
rafael cortes <ra.cortes.carrasco@gmail.com>
2022-09-14 21:59:36 -0300

    no puedo configurar el subtotal

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
-const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 class StatusCarrito {
-   constructor (nombre, precio, cantidad, imagen, id){
+   constructor(nombre,precio,cantidad,imagen,id,subtotal) {
       this.nombre = nombre;
       this.precio = precio;
-      this.imagen= imagen;
-      this.cantidad= 1;
+      this.imagen = imagen;
+      this.cantidad = 1;
       this.id = id;
+      this.subtotal= precio;
    }
 }
 
 let contenerdoproductos = document.getElementById("contenedorProducto");
 
 function cardsProductos(arrayProductos) {
-   for(let productos of arrayProductos) {
-      let div = document.createElement("div");  
+   for (let productos of arrayProductos) {
+      let div = document.createElement("div");
 
       div.className = 'consola__card-ps5';
 
@@ -28,44 +29,25 @@ function cardsProductos(arrayProductos) {
                         </div> `
 
       contenerdoproductos.appendChild(div);
- }
+   }
 }
 cardsProductos(productos);
 
-let botones = document.querySelectorAll(".boton__productos");  
-
-botones.forEach(elemento => {
-   elemento.addEventListener("click", agregarCarrito);
-})
-
-function agregarCarrito(e) {
-  
-   let imagen = e.target.parentNode.children[0].src;
-   let nombre = e.target.parentNode.children[1].textContent;
-   let precio = e.target.parentNode.children[2].textContent;  
-   let id = e.target.parentNode.children[0].alt;
-
-   const productoproductos = new StatusCarrito(imagen, nombre, precio, id);
-   carrito.push(productoproductos);
-
-   localStorage.setItem("carrito", JSON.stringify(carrito));
-}
 
 // Cargado PopUp - Toastify //
 let btnPopUp = document.querySelectorAll('.boton__productos');
 
 btnPopUp.forEach(btn => {
-   btn.addEventListener("click", () => {
+   btn.addEventListener("click",() => {
       Toastify({
-         text: "Producto agregado al carrito 👍 🛒", 
+         text: "Producto agregado al carrito 👍 🛒",
          style: {
-           background: "linear-gradient(to right, #00b09b, #96c93d)",
-           color: "white",
-           padding: "10px",
-           
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+            color: "white",
+            padding: "10px",
+
          }
-       }).showToast();
+      }).showToast();
    })
 })
-
 
--- js/carrito.js
+++ js/carrito.js
@@ -1,19 +1,29 @@
-carrito = JSON.parse(localStorage.getItem("carrito")) ;
-
 let tbody = document.querySelector("#tbody");
+carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+carrito.length > 0 && rellenarCarrito(carrito)
+
+//Obtengo los botones y les asigno el evento
+let botones = document.querySelectorAll(".boton__productos");
+
+console.log(botones);
+botones.forEach(elemento => {
+    elemento.addEventListener("click",agregarCarrito);
+})
+
+//Acá creo la función de rellenar el carrito
 
 function rellenarCarrito(arrayProductos) {
-    
-    for(let producto of arrayProductos){
-
+    tbody.innerHTML = ''
+    for (let producto of arrayProductos) {
         let row = document.createElement("tr");
 
 
         row.innerHTML = `
                         <td>${producto.nombre}</td>
-                        <td>$${producto.precio}</td>
+                        <td>${producto.precio}</td>
                         <td>${producto.cantidad}</td>
-                        <td>$</td>
+                        <td>${producto.subtotal}</td>
                         <td>Eliminar</td>
                         `
         tbody.appendChild(row);
@@ -21,4 +31,30 @@ function rellenarCarrito(arrayProductos) {
     }
 }
 
-rellenarCarrito(carrito);
+
+function agregarCarrito(e) {
+
+    let index = carrito.findIndex(productos => productos.id == e.target.parentNode.children[0].alt);
+
+
+    let imagen = e.target.parentNode.children[0].src;
+    let nombre = e.target.parentNode.children[1].textContent;
+    let precio = e.target.parentNode.children[2].textContent;
+    let id = e.target.parentNode.children[0].alt;
+    
+    if(index == -1){
+        const productoproductos = new StatusCarrito(nombre,precio,imagen,1,id);
+        carrito.push(productoproductos);
+    }else {
+        carrito[index].cantidad++;
+        carrito[index].subtotal = carrito[index].precio * carrito[index].cantidad;
+    }
+
+    
+
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+    rellenarCarrito(carrito);
+}
+
+
+
